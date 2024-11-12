774a4d7e18d9417c1f697c842b7c840fbde40ad4
helonely <idravil1211@gmail.com>
2024-11-13 04:55:48 +0500

    added urls

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,49 @@
 """
-URL configuration for config project.
+Конфигурация URL для проекта конфигурации.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Список `urlpatterns` направляет URL в представления. Для получения дополнительной информации см.:
+https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Примеры:
+Функциональные представления
+1. Добавьте импорт: from my_app import views
+2. Добавьте URL в urlpatterns: path('', views.home, name='home')
+Представления на основе классов
+1. Добавьте импорт: from other_app.views import Home
+2. Добавьте URL в urlpatterns: path('', Home.as_view(), name='home')
+Включение другого URLconf
+1. Импортируйте функцию include(): from django.urls import include, path
+2. Добавьте URL в urlpatterns: path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("habits/", include("habits.urls", namespace="habits")),
+    path("users/", include("users.urls", namespace="users")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
